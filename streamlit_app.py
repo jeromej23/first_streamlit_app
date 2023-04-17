9da47dfacb373883273e4fb1931bb1592580eebb
jeromej23 <131065534+jeromej23@users.noreply.github.com>
2023-04-18 08:25:53 +1000

    Update streamlit_app.py
    Updated title and added new item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Mom''s New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boilded Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
